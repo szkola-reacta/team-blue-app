fe3b66eedc99eb5adbdd988ab091f47ff43cb43e
Kamil Zieliński <kamil.zielinski@miquido.com>
2020-05-28 15:02:11 +0200

    feat(api): API init

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
+import { makeServer } from "./server"
+
+makeServer({ environment: 'development' })
 
 ReactDOM.render(
   <React.StrictMode>
