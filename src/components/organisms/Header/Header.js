9df9fb15945b3102388f4e3e51153552f0eb3d5c
Piotr Łosiak <piotr.losiak96@gmail.com>
2020-05-30 14:49:20 +0200

    rename logo

--- src/components/organisms/Header/Header.js
+++ src/components/organisms/Header/Header.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Link } from '@reach/router';
 
 import Image from 'react-bootstrap/Image';
-import logoPlaceholder from '../../../images/logo-placeholder.svg';
+import logo from '../../../images/logo-placeholder.svg';
 import './Header.scss';
 
 function Header() {
     return(
         <div className="header">
-            <Image src={logoPlaceholder} alt="logo" />
+            <Image src={logo} alt="logo" />
             <Link to="signup" className="btn btn-primary">Sign up</Link>
         </div>
     );
