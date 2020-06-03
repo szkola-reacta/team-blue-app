81a2f61ffcdb098c45d1a6ea499f50e0c76f7ca6
Piotr Łosiak <piotr.losiak96@gmail.com>
2020-06-03 23:32:03 +0200

    move content, footer, header components from organisms folder to components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './App.scss';
 
-import Header from './Components/organisms/Header';
-import Content from './Components/organisms/Content';
-import Footer from './Components/organisms/Footer';
+import Header from './Components/Header';
+import Content from './Components/Content';
+import Footer from './Components/Footer';
 import Home from './Components/Pages/Home/';
 
 function App() {
