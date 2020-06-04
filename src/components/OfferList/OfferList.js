3910621de5f848347bc1f02a9bd10ce578c6b207
Marcin Wijas <marcinw1577@gmail.com>
2020-06-04 16:21:45 +0200

    fix import, remove unnecessary code, fix some things in scss

--- src/components/OfferList/OfferList.js
+++ src/components/OfferList/OfferList.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import Offer from '../OfferTile';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import Offer from '../OfferTile';
 import './OfferList.scss';
 import '../OfferTile/Offer.scss';
 
 function OfferList({ offers }) {
   return (
-    <Container className="offer-list" fluid>
+    <Container className="offer-list">
       <Row className="offer-row">
         {offers.map((element, index) =>
-          <Col key={`offer-${ index }`} xs={12} sm={12} md={6} lg={3} xl={3}>
+          <Col key={offers.id}>
             <Offer {...element} />
           </Col>
         )}
