f209c101361bd8ac0d551c6dbf3f5dfcf8359a1c
Piotr Łosiak <piotr.losiak96@gmail.com>
2020-06-05 00:09:49 +0200

    fix indentation size

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -4,20 +4,20 @@ import Search from '../../Search';
 import CategoryList from '../../CategoryList';
 
 function Home() {
-    const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-    useEffect(() => {
-        fetch('/api/categories')
-          .then(response => response.json())
-          .then(data => setCategories(data));
-      }, [])
+  useEffect(() => {
+    fetch('/api/categories')
+    .then(response => response.json())
+    .then(data => setCategories(data));
+    }, [])
 
-    return (
-        <div className="page">
-            <Search />
-            <CategoryList categories={categories} />
-        </div>
-    );
+  return (
+    <div className="page">
+      <Search />
+      <CategoryList categories={categories} />
+    </div>
+  );
 }
 
 export default Home;
--- src/components/pages/Home/Home.test.js
+++ src/components/pages/Home/Home.test.js
@@ -4,10 +4,10 @@ import { render } from '@testing-library/react';
 import Home from './Home';
 
 test('render Home page', () => {
-    const { container } = render(<Home />);
-    const searchContainer = container.querySelector('.search');
-    const categoryListContainer = container.querySelector('.category-list');
+  const { container } = render(<Home />);
+  const searchContainer = container.querySelector('.search');
+  const categoryListContainer = container.querySelector('.category-list');
 
-    expect(searchContainer).toBeInTheDocument();
-    expect(categoryListContainer).toBeInTheDocument();
+  expect(searchContainer).toBeInTheDocument();
+  expect(categoryListContainer).toBeInTheDocument();
 });
